f30dd2936c8610126d0fc6452652e6b3256b8c23
lzs <forlzs@163.com>
2023-03-02 20:32:19 +0800

    修改注释

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,7 @@
     首次访问
     刷新页面
     切换到当前文章的其它章节
-    切换锚点，URL 会变化，所以会触发路由变化事件。
+    切换锚点，URL 会变化，所以会触发路由变化事件。[已优化]
     所以，当用户完整得浏览完一篇博客时，可能会触发多次上报。
  */
 export default ({ router }) => {
@@ -11,7 +11,6 @@ export default ({ router }) => {
      * 路由切换事件处理
      */
     router.beforeEach((to, from, next) => {
-      console.log("切换路由", to.fullPath, from.fullPath);
       
       //触发百度的pv统计
       if (typeof _hmt != "undefined") {
